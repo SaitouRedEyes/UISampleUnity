ccbe93d32dcc5b8fc12fd61b82e3c2fcc09a0b8b
SaitouRedEyes <zeca.saitou@gmail.com>
2016-05-03 22:04:25 -0300

    UI example Unity
    This is a example of some UI objects on Unity.

---
+++ Assets/Scripts/ChangeAlpha.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeAlpha : MonoBehaviour
+{
+    public void ChangeObjectColor(string color)
+    {
+        string[] rgb = color.Split(',');
+
+        this.GetComponent<Image>().color = new Color(int.Parse(rgb[0]), 
+                                                     int.Parse(rgb[1]), 
+                                                     int.Parse(rgb[2]), 
+                                                     this.GetComponent<Image>().color.a);
+    }
+    
+    public void ChangeObjectAlpha(float value)
+    {
+        this.GetComponent<Image>().color = new Color(this.GetComponent<Image>().color.r,
+                                                 this.GetComponent<Image>().color.g,
+                                                 this.GetComponent<Image>().color.b,
+                                                 value);
+    }
+}
+
+
+
+
+
+
+
